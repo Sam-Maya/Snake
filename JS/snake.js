167de479fb03477609a3226a36bcca73e41bbd7a
Sam Maya <samuelmaya112@gmail.com>
2022-06-01 12:47:17 -0400

    got snake to eat food

--- JS/snake.js
+++ JS/snake.js
@@ -9,9 +9,16 @@ let context = undefined;
 let snakeX = blockSize * 10;
 let snakeY = blockSize * 10;
 
+//snake body
+let body = [];
+
 //food
 let foodX = undefined;
 let foodY = undefined;
+
+//moving the snake
+let velocityX = 0;
+let velocityY = 0;
 
 
 window.onload = function() {
@@ -21,22 +28,47 @@ window.onload = function() {
     context = board.getContext('2d'); // lets you "draw" on the board
 
     placeFoodRandomly();
-    update();
+    document.addEventListener("keydown", move)
+    setInterval(update, 1000/10);
 }
 
 function update(){
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
 
-    context.fillStyle = "#05c71e";
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
     context.fillStyle = "red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
 
+    if(snakeX === foodX && snakeY === foodY){
+        placeFoodRandomly();
+    }
+
+    context.fillStyle = "#05c71e";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
 }
 
 function placeFoodRandomly(){
     foodX = Math.floor(Math.random() * cols) * blockSize;
     foodY = Math.floor(Math.random() * rows) * blockSize;
+}
+
+function move(input){
+    if(input.code == "ArrowUp" && velocityY !== 1){
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if(input.code == "ArrowDown" && velocityY !== -1){
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if(input.code == "ArrowLeft" && velocityX !== 1){
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if(input.code == "ArrowRight" && velocityX !== -1){
+        velocityX = 1;
+        velocityY = 0;
+    }
 }
